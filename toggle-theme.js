21252131d75d3cc4c8c87d9e78b1be93eb88558b
simon020286 <simon020286@users.noreply.github.com>
2024-06-05 07:49:44 +0000

    Astro tutorial 02 (#8)
    Co-authored-by: codewithheart <codewithheart.dev> simon020286/code-with-heart@00b681d5db1a5f891051241e30f6b7bbd35660c9

--- toggle-theme.js
+++ toggle-theme.js
@@ -44,6 +44,7 @@ function reflectPreference() {
       .querySelector("meta[name='theme-color']")
       ?.setAttribute("content", bgColor);
   }
+  updatePictures(themeValue);
 }
 
 // set early so no page flashes / CSS is made aware
@@ -67,6 +68,30 @@ window.onload = () => {
   document.addEventListener("astro:after-swap", setThemeFeature);
 };
 
+function updatePictures(theme) {
+  const pictures = document.querySelectorAll("picture");
+  pictures.forEach(picture => updatePicture(picture, theme));
+}
+
+function updatePicture(picture, theme) {
+  const sources = picture.querySelectorAll('source[media*="prefers-color-scheme"], source[data-media*="prefers-color-scheme"]');
+  sources.forEach((source) => {
+      // Preserve the source `media` as a data-attribute
+      // to be able to switch between preferences
+      if (source?.media.includes('prefers-color-scheme')) {
+        source.dataset.media = source.media
+      }
+ 
+      // If the source element `media` target is the `preference`,
+      // override it to 'all' to show
+      // or set it to 'none' to hide
+      if (source?.dataset.media.includes(theme)) {
+        source.media = 'all'
+      } else if (source) {
+        source.media = 'none'
+      }
+    });
+}
 // sync with system changes
 window
   .matchMedia("(prefers-color-scheme: dark)")
